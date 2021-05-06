6cc43c37ba8cb02fed05253ea8acef9eb1b01910
Theo Martin Santoso <79010034+theomartinsantoso@users.noreply.github.com>
2021-05-06 10:44:21 +0700

    Create LuasLingkaran.rb

---
+++ LuasLingkaran.rb
@@ -0,0 +1,11 @@
+    phi = 3.14
+    puts "========================="
+    puts "Menghitung Luas Lingkaran"
+    puts "========================="
+    puts
+    printf "Masukkan panjang jari-jari lingkaran: "
+    r = gets.to_i
+    luas = phi*r*r
+    puts
+    puts "Luas Lingkaran adalah "+luas.to_s
+    puts
