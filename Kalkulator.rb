40ef36d8d70ac8dfa8e846b2348f190fba12206a
Theo Martin Santoso <79010034+theomartinsantoso@users.noreply.github.com>
2021-05-06 10:40:18 +0700

    Create Kalkulator.rb

---
+++ Kalkulator.rb
@@ -0,0 +1,16 @@
+a = 10
+b = 3
+c = 3.0
+
+puts "a \t= #{a}"
+puts "b \t= #{b}"
+puts "c \t= #{c}"
+
+print "\nOperasi aritmetika: \n"
+puts  "a + b \t= #{a + b}"
+puts  "a - b \t= #{a - b}"
+puts  "a * b \t= #{a * b}"
+puts  "a / b \t= #{a / b}"
+puts  "a / c \t= #{a / b}"
+puts  "a % b \t= #{a % b}"
+puts  "a ** b \t= #{a ** b}"
