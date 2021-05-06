22370a1d73655b26d8a2dd1f8a8087f79b893858
Theo Martin Santoso <79010034+theomartinsantoso@users.noreply.github.com>
2021-05-06 12:53:31 +0700

    Create BelahKetupat.rb

---
+++ BelahKetupat.rb
@@ -0,0 +1,13 @@
+    puts
+    puts "============================="
+    puts "Menghitung Luas Belah Ketupat"
+    puts "============================="
+    puts
+    printf "Masukkan diagonal 1: "
+    d1 = gets.to_i
+    printf "Masukkan diagonal 2: "
+    d2 = gets.to_i
+    luas = d1*d2/2
+    puts
+    puts "Luas Belah Ketupat adalah "+luas.to_s
+    puts
